34459d7e57adbaa2d82a45c4a8d0b0e24ab82826
Nirmal Ariyathilake <nirmal.ariyathilake@gmail.com>
2022-12-09 18:02:18 +0530

    Shared package with user model

---
+++ server/packages/shared/lib/src/models.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'models.freezed.dart';
+part 'models.g.dart';
+
+@freezed
+class User with _$User {
+  const factory User({
+    String? id,
+    required String email,
+  }) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+}
